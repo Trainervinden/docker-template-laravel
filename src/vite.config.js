660b16d37ea50abbc91ada76af47ccc24be32f59
Your Name <you@example.com>
2023-02-10 11:20:03 +0100

    betere vite config

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            server: {
-                watch: {
-                    usePolling: true,
-                },
-                host: true, // needed for the Docker Container port mapping to work
-                strictPort: true,
-                port: 5173, // you can replace this port with any port
-            }
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/fa/all.min.js'
+            ],
+            refresh: true
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
